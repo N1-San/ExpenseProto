b6a44ecd8ba813ea35b60bb69f09e36e474aee57
N1-San <33519431+N1-San@users.noreply.github.com>
2025-05-02 15:35:50 +0530

    routes refactor

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 // Public welcome page
 Route::get('/', function () {
@@ -20,23 +20,27 @@ Route::get('/', function () {
 
 // Protected routes - only for authenticated users
 Route::middleware(['auth'])->group(function () {
-    
+
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
 
-    // Accounts
-    Route::middleware(['auth', 'can:view accounts'])->get('/accounts', [AccountController::class, 'index'])->name('accounts');
-    Route::get('/accounts/create', [AccountController::class,'create'])->name('accounts.create');
-    Route::post('/accounts/store', [AccountController::class,'store'])->name('accounts.store');
-    Route::delete('/accounts/delete/{account}', [AccountController::class,'destroy'])->name('accounts.destroy');
-    Route::get('/accounts/edit/{account}', [AccountController::class,'edit'])->name('accounts.edit');
-    Route::put('/accounts/update', [AccountController::class,'update'])->name('accounts.update');
-    Route::post('/accounts/toggleActive/{account}', [AccountController::class,'toggleActive'])->name('accounts.toggleActive');
-    
+    //Accounts
+    Route::prefix('accounts')->name('accounts.')->group(function () {
+        Route::middleware(['auth', 'can:view accounts'])->get('/', [AccountController::class, 'index'])->name('index');
+        Route::get('/create', [AccountController::class, 'create'])->name('create');
+        Route::post('/store', [AccountController::class, 'store'])->name('store');
+        Route::delete('/delete/{account}', [AccountController::class, 'destroy'])->name('destroy');
+        Route::get('/edit/{account}', [AccountController::class, 'edit'])->name('edit');
+        Route::put('/update', [AccountController::class, 'update'])->name('update');
+        Route::post('/toggleActive/{account}', [AccountController::class, 'toggleActive'])->name('toggleActive');
+    });
+
     // Transactions
-    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions');
-    Route::get('/transactions/create', [TransactionController::class,'create'])->name('transactions.create');
-    Route::post('/transactions/store', [TransactionController::class,'store'])->name('transactions.store');
+    Route::prefix('transactions')->name('transactions.')->group(function () {
+        Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions');
+        Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
+        Route::post('/transactions/store', [TransactionController::class, 'store'])->name('transactions.store');
+    });
 });
 
 
@@ -51,11 +55,11 @@ Route::view('/user', 'pages.user')->name('user');
 
 
 // Route::get('/dashboard', function () {
-    //     return view('dashboard');
-    // })->middleware(['auth', 'verified'])->name('dashboard');
-    
-    // Route::get('/', function () {
-        //     return view('pages.dashboard');
-        // })->name('dashboard');
-        
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('pages.dashboard');
+// })->name('dashboard');
+
 // Route::view('/accounts', 'pages.accounts')->name('accounts');
