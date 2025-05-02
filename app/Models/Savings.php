25e26dd8f60f9f6843f1fd7c905f73e2d799c59f
N1-San <33519431+N1-San@users.noreply.github.com>
2025-05-02 15:33:08 +0530

    model and migrations setup for savings

--- app/Models/Savings.php
+++ app/Models/Savings.php
@@ -5,17 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
-class Account extends Model
+class Savings extends Model
 {
 
     use SoftDeletes;
-
     protected $fillable = [
         'name',
         'amount',
-        'is_active',
-    ];
+    ] ;
     public function transactions()
     {
         return $this->morphMany(Transaction::class, 'transactionable');
